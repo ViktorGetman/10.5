2973ec04a2f940e6dd1ddbeb8995cfc876b38136
ViktorGetman <123113442+ViktorGetman@users.noreply.github.com>
2023-04-07 19:50:39 +0300

    Добавьте файлы проекта.

---
+++ 10.5/Program.cs
@@ -0,0 +1,47 @@
+﻿using _10._5;
+
+class Program
+{
+    static ILogger Logger { get; set; }
+    static void Main()
+    {
+        Logger = new Logger();
+
+        var calculator = new Calculator(Logger);
+
+        calculator.Calc();
+
+
+        Console.ReadKey();
+    }
+}
+public interface ILogger
+{
+    void Event(string message);
+    void Eror(string message);
+}
+
+public class Logger : ILogger
+{
+    void ILogger.Eror(string message)
+    {
+        ConsoleColor mainColor = Console.ForegroundColor;
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(message);
+        Console.ForegroundColor = mainColor;
+    }
+
+    void ILogger.Event(string message)
+    {
+        ConsoleColor mainColor = Console.ForegroundColor;
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine(message);
+        Console.ForegroundColor = mainColor;
+    }
+}
+
+public interface IСalculator
+
+{
+    void Calc();
+}
---
+++ 10.5/Сalculator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10._5
+{
+    public class Calculator : IСalculator
+    {
+        ILogger Logger { get; }
+        public Calculator(ILogger logger)
+        {
+            Logger = logger;
+        }
+
+        public void Calc()
+        {
+            while (true)
+            {
+                try
+                {
+                    Logger.Event("Введите первое число.");
+                    int a = Int32.Parse(Console.ReadLine());
+                    Logger.Event("Введите второе число.");
+                    int b = Int32.Parse(Console.ReadLine());
+
+                    int c = a + b;
+                    Logger.Event($"Результат сложения чисел равен: \n{c}");
+                    return;
+                }
+                catch (FormatException ex)
+                {
+                    Logger.Eror("Введено неверное значение.");
+                }
+            }
+        }
+    }
+}
